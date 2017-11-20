5d7795843fd5f898940e1dd89c87907869e98e55
ComlOnline <ComlOnline@users.noreply.github.com>
2017-11-20 00:21:23 +0000

    Options for powershell
    Also there is an issue with icecast giving non zero listener times for 404 responces needto work on the regex for just 200

--- README.md
+++ README.md
@@ -9,10 +9,21 @@ Correct function is ```./pplreport.sh <year> <quarter> <mount>```
 ## Windows Systems using powershell
 You may need to run Set-ExecutionPolicy Unrestricted from an elevated prompt to enable powershell scripts
 
+Correct function is ```pplreport.ps1 -year <year> -quarter <quarter> -mount <mount>```
+Where:
+- Year is a Year. Example: 2017
+- Quarter is a number 1, 2, 3 or 4. Example: For April, May and June this would be 2.
+- Mount is your mount without the slash. Example: MyMount.ogg
+
 ### ToDo
-- [ ] Make the Powershell script take options
+- [X] Make the Powershell script take options (And its validates what it can)
 - [ ] Make them feed to influxDB
-- [ ] Pretty Outout
-- [ ] Optomize powershell
-- [ ] Automated reporting to PPL?
+- [ ] Pretty Output
+- [ ] Optomize powershell (Make it only search relevent log files)
 - [ ] Email reports
+- [ ] In email remind of PPl details and Link
+- [ ] Fix variance in numbers between scripts
+
+### Helpful
+https://gist.github.com/sunnyone/4291248
+https://stackoverflow.com/questions/9439210/how-can-i-make-this-powershell-script-parse-large-files-faster
--- pplreport.ps1
+++ pplreport.ps1
@@ -1,12 +1,54 @@
-$pattern=@'
-(Jan|Feb|Mar)\/2017.*\/HullfireRadio\.mp3\s.*\s200\s.*\s".*"\s".*"\s[^0]\d*
-'@
+ param (
+    [Parameter(Mandatory=$true)]
+	[int]
+	$year
+,
+    [Parameter(Mandatory=$true)]
+	[ValidateRange(1,4)]
+	[int]
+	$quarter
+,
+    [Parameter(Mandatory=$true)]
+	[string]
+	$mount
+ )
+
+$mount=[Regex]::Escape($mount)
+
+If ($quarter -eq 1) {set-variable -Name months -value 'Jan|Feb|Mar'}
+ElseIf ($quarter -eq 2) {set-variable -Name months -value 'Apr|May|Jun'}
+ElseIf ($quarter -eq 3) {set-variable -Name months -value 'Jul|Aug|Sep'}
+ElseIf ($quarter -eq 4) {set-variable -Name months -value 'Oct|Nov|Dec'}
+
+$pattern=@"
+($months)\/2017.*\/$mount\s.*\s200\s.*\s".*"\s".*"\s[^0]\d*
+"@
 
 $target = @()
 
+Write-Progress -Activity "Searching..." `
+	-Status "Please wait."
+	
 $results=Select-String -Path "access.log*" -Pattern $pattern
-# $results.Matches.Value.Split()[-1]
+
+$items = $results.Matches.Value.Count
+If ($items -eq 0) {"Nothing Found for quarter $quarter $year"; BREAK}
+
+$i = 0
+$percent = 0
+
+Write-Progress -Activity "Counting..." `
+	-Status "Please wait."
+
 $results.Matches.Value | foreach {
+	<# $percent = [math]::Round($i/$items*100)
+	Write-Progress -Activity "Counting..." `
+	-PercentComplete $percent -CurrentOperation `
+	"$percent% complete" `
+	-Status "Please wait."
+	$i++ 
+	This is commented out as it makes the script take a lot longer#>
 	$target += $_.Split(' ')[-1]
 }
-($target | Measure-Object -Sum).sum/60/60
+$hours=[math]::Round(($target | Measure-Object -Sum).sum/60/60,2)
+Write-Output "Total listener hours for quarter $quarter $year`: $hours"
